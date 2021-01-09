a2f4c24f6bf33237b0c5e7122517fa81f7c2b159
Mahan Niknafs <mahan.nk@gmail.com>
2021-01-09 15:02:39 -0500

    Update model.py

--- app/dependencies/model.py
+++ app/dependencies/model.py
@@ -1,6 +1,8 @@
 from tensorflow.keras.applications.nasnet import NASNetMobile, preprocess_input, decode_predictions
 import numpy as np
 import cv2
+from io import BytesIO
+from PIL import Image
 
 __model = None
 
@@ -10,11 +12,15 @@ def set_predictor():
 
 
 def load_image(image):
-    img = np.fromstring(image, np.uint8)
-    img = cv2.imdecode(img, cv2.IMREAD_COLOR)
-    img = cv2.resize(img, dsize=(224, 224), interpolation=cv2.INTER_CUBIC)
+    # img = np.fromstring(image, np.uint8)
+    # img = cv2.imdecode(img, cv2.IMREAD_COLOR)
+    # img = cv2.resize(img, dsize=(224, 224), interpolation=cv2.INTER_CUBIC)
+    # img = np.expand_dims(img, 0)
+    # return img      
+    img = Image.open(BytesIO(image)) 
+    img = np.asarray(img.resize((224, 224)))[..., :3]
     img = np.expand_dims(img, 0)
-    return img       
+    return img
     
 def classify_image(image):
     print('==============================================')
