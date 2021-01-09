dd9b399fc8736d949804c16fcc9631a86ecf78aa
Mahan Niknafs <mahan.nk@gmail.com>
2021-01-09 12:52:14 -0500

    update requirements

--- app/api/api_v1/endpoints/predictor.py
+++ app/api/api_v1/endpoints/predictor.py
@@ -1,10 +1,10 @@
 from fastapi import APIRouter, status, Response
 from fastapi import UploadFile, File
-from ....dependencies.model import Prediction
+# from ....dependencies.model import Prediction
 import numpy as np
 
 router = APIRouter()
-predictor = Prediction()
+# predictor = Prediction()
 
 @router.post("/")
 async def predict(reponse: Response, image: UploadFile = File(...)):
@@ -14,8 +14,10 @@ async def predict(reponse: Response, image: UploadFile = File(...)):
         return {'message' : "File format not supported"}
     
     else:
-        img = predictor.load_image(await image.read())
-        results = predictor.predict(img)
+        # img = predictor.load_image(await image.read())
+        # results = predictor.predict(img)
         reponse.status_code = status.HTTP_202_ACCEPTED
+        results = 'Done!'
+
 
     return {"message" : "success", "predictions" : results}
