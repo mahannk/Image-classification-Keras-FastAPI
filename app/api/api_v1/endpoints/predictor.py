31d628511324b00831dbfb85f08e4eae73814eed
Mahan Niknafs <mahan.nk@gmail.com>
2021-01-09 18:21:54 -0500

    Update predictor.py

--- app/api/api_v1/endpoints/predictor.py
+++ app/api/api_v1/endpoints/predictor.py
@@ -1,23 +1,29 @@
 from fastapi import APIRouter, status, Response
 from fastapi import UploadFile, File
-from ....dependencies.model import Prediction
+
 import numpy as np
 
 router = APIRouter()
-predictor = Prediction()
+predictor = None 
 
 @router.post("/")
 async def predict(reponse: Response, image: UploadFile = File(...)):
+    global predictor
 
     if image.content_type not in ('image/jpeg', 'image/jpg', 'image/png'):
         reponse.status_code = status.HTTP_415_UNSUPPORTED_MEDIA_TYPE
         return {'message' : "File format not supported"}
     
     else:
+        if predictor is None:
+            from ....dependencies.model import Prediction
+            predictor = Prediction()
+
+
         img = predictor.load_image(await image.read())
         results = predictor.predict(img)
 
         reponse.status_code = status.HTTP_202_ACCEPTED
 
 
-    return {"message" : "success", "predictions" : "DONE"}
+    return {"message" : "success", "predictions" : results}
